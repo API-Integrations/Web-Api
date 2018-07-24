7c89af87e404fe2f10cfb58f7b3f4cbded07cb64
Shubshuhovjit DasguptaS <shuSsbhojit.dasgupta@gmail.com>
2018-07-24 13:26:01 +0530

    Add project files.

---
+++ ConsoleClient/ConsoleClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml;
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue
+                ("application/xml"));
+            var result = client.GetAsync(new Uri("http://localhost:52444/api/Videos")).Result;
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                var doc = XDocument.Load(result.Content.ReadAsStreamAsync().Result);
+                var ns = (XNamespace)"http://schemas.datacontract.org/2004/07/Videos.Models";
+                foreach (var title in doc.Descendants(ns + "Title"))
+                {
+                    Console.WriteLine(title.Value);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Videos/Models/VideoDb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+namespace Videos.Models
+{
+    public class VideoDb: DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+    }
+}
